bda339207bd4a59f57559b548bd144609bf707df
sUty <496678692@qq.com>
2016-12-12 22:03:37 +0800

    Mr.Wang

---
+++ src/Yuanzhangdeanniu.java
@@ -0,0 +1,56 @@
+package 儿科门诊医生诊疗操作;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import 儿科门诊医生诊疗操作.LoginDemo.B1;
+public class Yuanzhangdeanniu extends JFrame{
+private JButton myButton1,myButton2,myButton3,myButton4;
+public Yuanzhangdeanniu(){
+	super("院长的查询按钮");
+	setBounds(0,0,1000,645);
+	setLayout(null);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setVisible(true);
+	myButton1=new JButton("科室挂号量和总金额");
+	myButton2=new JButton("科室挂号量和总金额(饼图)");
+	myButton3=new JButton("各药品库存");
+	myButton4=new JButton("各医生就诊数量和金额");
+	add(myButton1);
+	add(myButton2);
+	add(myButton3);
+	add(myButton4);
+	myButton1.setBounds(0,0,500,300);
+	myButton2.setBounds(500,0,500,300);
+	myButton3.setBounds(0,300,500,300);
+	myButton4.setBounds(500,300,500,300);
+	myButton1.addActionListener(new myButton1());
+	myButton3.addActionListener(new myButton3());
+	myButton4.addActionListener(new myButton4());
+}
+class myButton1 implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+		new guahaoliang().setVisible(true);
+		dispose();
+	}
+}
+class myButton3 implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+		new yaopinkucun().setVisible(true);
+		dispose();
+	}
+}
+class myButton4 implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+		new yisheng().setVisible(true);
+		dispose();
+	}
+}
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Yuanzhangdeanniu f=new Yuanzhangdeanniu();
+	}
+
+}
