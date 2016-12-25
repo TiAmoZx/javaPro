08ba61fad42a1a4101fb18b87f6008a9b34826af
llzz77 <1367621738@qq.com>
2016-12-25 13:46:38 +0800

    功能修改
    点击提交按钮后，能将文本框中的数据输入到相应的数据库中

--- src/Order.java
+++ src/Order.java
@@ -3,7 +3,12 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.mysql.jdbc.Statement;
+
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -18,11 +23,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.Random;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import java.lang.Thread;
 
 
 
@@ -42,8 +51,8 @@ public class Order extends JFrame {
 	private JTextField orderroom;
 	private JTextField ordertime;
 	private JTextField innumber;
-	Thread thread1=new Thread();
-	
+
+	Thread thread=new Thread();
 	/**
 	 * Launch the application.
 	 */
@@ -59,27 +68,21 @@ public class Order extends JFrame {
 				}
 			}
 		});
+	
 	}
-
 	/**
 	 * Create the frame.
 	 */
 	public Order() {
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(200, 150, 462, 310);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		  double sumL = 0 ;  
-	        byte[] sendBytes = null;  
-	        Socket socket = null;  
-	        DataOutputStream dos = null;  
-	        FileInputStream fis = null;  
-	        boolean bool = false;
-	        String filePath = "D:/temp/"+GetDate.getDate()+"SJ"+new Random().nextInt(10000)+".txt";  
-		
-		
+
 		JLabel label = new JLabel("\u6027\u522B");
 		label.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		label.setBounds(24, 71, 54, 15);
@@ -145,74 +148,11 @@ public class Order extends JFrame {
 		contentPane.add(ordertime);
 		ordertime.setColumns(10);
 		
-		JButton button = new JButton("\u63D0\u4EA4");
-		button.setForeground(new Color(102, 0, 102));
-		button.setFont(new Font("微软雅黑", Font.PLAIN, 12));
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				thread1.start();
-				
-				dispose();
-				Button btn=new Button();
-				btn.setVisible(true);
-			}
 			
-		});
-		
-		thread1=new Thread(new Runnable(){
-		   public void run(){
-				   try{
-					 File file=new File("word.txt"); 
-					 FileWriter out=new FileWriter(file);
-              	     
-                    String s13=label_7.getText();
-              	     String s=name.getText();
-                    String s7=label_1.getText();
-               	 String s1=age.getText();
-              	     String s8=label_2.getText();
-                	 String s2=sex.getText();
-           	         String s9=label.getText();           	  
-             	     String s3=phonenumber.getText();
-          	         String s10=label_3.getText();      	  
-              	     String s4=orderroom.getText();
-   	                 String s11=label_4.getText();  	    	         
-   	                 String s5=ordertime.getText();
-	                 String s12=label_5.getText();	  
-	                 String s6=innumber.getText();	           	             
-	                 out.write(s7+"\t");  out.write(s+"\r\n");
-                 out.write(s8+"\t");  out.write(s1+"\r\n");
-	                 out.write(s9+"\t");  out.write(s2+"\r\n");
-	                 out.write(s10+"\t"); out.write(s3+"\r\n");
-                out.write(s11+"\t"); out.write(s4+"\r\n");
-	                 out.write(s12+"\t"); out.write(s5+"\r\n");
-	                 out.write(s13+"\t"); out.write(s6+"\r\n");
-	                 out.close();
-	                 name.setText(""); 
-	 				 sex.setText("");	 				
-	 				 age.setText(""); 
-	 				 innumber.setText(""); 
-				     phonenumber.setText("");	
-	 			     orderroom.setText("");	
-	 			   	 ordertime.setText(""); 	
-                  }
-                  catch(Exception e1)
-				   {e1.printStackTrace();
-                    }
-				
-			}
-			
-		});
-		
-		
-		
-		
 		innumber = new JTextField();
 		innumber.setBounds(321, 43, 100, 21);
 		contentPane.add(innumber);
 		innumber.setColumns(10);
-		button.setBounds(51, 232, 93, 23);
-		contentPane.add(button);
 		
 		JLabel label_6 = new JLabel("\u533B\u9662\u9884\u7EA6\u754C\u9762");
 		label_6.setForeground(new Color(204, 0, 102));
@@ -225,11 +165,17 @@ public class Order extends JFrame {
 		button_1.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+				dispose();
+				Log yu=new Log();
+				yu.setVisible(true);
 			}
 		});
 		button_1.setBounds(328, 232, 93, 23);
 		contentPane.add(button_1);
+		
+		
+	
+		
 		
 		JButton button_2 = new JButton("重置");
 		button_2.setForeground(new Color(204, 0, 102));
@@ -249,24 +195,63 @@ public class Order extends JFrame {
 		button_2.setBounds(202, 232, 93, 23);
 		contentPane.add(button_2);
 		
+		innumber = new JTextField();
+		innumber.setBounds(321, 43, 100, 21);
+		contentPane.add(innumber);
+		innumber.setColumns(10);
+		
+		
+	
+		thread=new Thread(new Runnable(){
+			public void run(){
+				  try{
+					String t;
+					String b;
+					String d;
+					String p;
+					String l;
+					String m;
+					String n;
+					t=name.getText();
+					b=innumber.getText();
+					d=sex.getText();
+					p=age.getText();
+					l=phonenumber.getText();
+					m=orderroom.getText();
+					n=ordertime.getText();
+					System.out.println("11"+t);
+					String SQL = " insert into Yuyue values('"+t+"','"+b+"','"+d+"','"+p+"','"+l+"','"+m+"','"+n+"') "; 
+			
+			         if(SqlHelper.executeUpdate(SQL))  
+			         {  
+			             System.out.println("插入成功 ");  
+			         }else  
+			         {  
+			             System.out.println("插入失败 ");  
+			         }  
+	         }
+			catch(Exception e){
+				e.printStackTrace();			
+			}	
+			}});
+	
+		JButton button_3 = new JButton("提交");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					 thread.start();
+					
+				}
+			
+		});
+		button_3.setBounds(50, 232, 93, 23);
+		contentPane.add(button_3);
+		
+		
+		
 		JLabel label_8 = new JLabel("");
 		label_8.setIcon(new ImageIcon("C:\\Users\\lenovo\\Desktop\\新建文件夹\\QQ图片20161214215736.jpg"));
 		label_8.setBounds(10, 10, 436, 261);
 		contentPane.add(label_8);
-	}
+		}
+		}
 
-	protected void close() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	protected void connect(InetSocketAddress inetSocketAddress) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	protected OutputStream getOutputStream() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
